1345d8422ffdc721e24cf6eb2fea2e0a31c18f79
edwardchalstrey1 <edwardchalstrey@gmail.com>
2025-03-29 18:26:29 +0000

    fix nav link

--- src/components/navbar/Nav.jsx
+++ src/components/navbar/Nav.jsx
@@ -29,7 +29,7 @@ export default function Nav({ posts }) {
     <>
       <nav className="container mx-auto top-0 z-50 absolute bg-primary dark:bg-dk-primary">
         <div className="w-full px-6 py-2 flex justify-between items-center">
-          <a className="font-bold text-2xl lg:text-4xl" href="/#">
+          <a className="font-bold text-2xl lg:text-4xl" href="/edchalstrey/#">
             <span className="text-secondary dark:text-dk-secondary">
               {"</" + extractInitials(info.name) + ">"}
             </span>

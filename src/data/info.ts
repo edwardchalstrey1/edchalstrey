5a9659dfcecf7d026580489d3d41137fc41628bf
edwardchalstrey1 <edwardchalstrey@gmail.com>
2025-03-29 16:32:43 +0000

    comment out unused

--- src/data/info.ts
+++ src/data/info.ts
@@ -10,40 +10,40 @@ export const info = {
 
   about: {
     description: `I'm a developer with a background in academic research, currently employed at The Alan Turing Institute in London. My career has spanned a variety of research disciplines and has given me a broad technical skillset in data science and software engineering. I'm passionate about using technology to solve complex problems and make a positive impact on society. On my website, you can find links to my software repositories, notebooks, publications and blogs, as well as episode links for The Turing Podcast which I co-host.`,
-    education: [
-      {
-        title: "Self-Taught Developer Extraordinaire",
-        date: "2000 - Present",
-        location: "Springfield Elementary School of Hard Knocks",
-        gpa: "4.0 (A+ in Pranks)",
-        thesis: "The Art of Subverting Expectations: A Practical Guide",
-      },
-    ],
-    experience: [
-      {
-        title: "Frontend Developer",
-        date: "2015 - Present",
-        location: "Krusty Burger Web Dev",
-        description:
-          "Crafted interactive web experiences using React and Redux. Often multitasked between coding and avoiding Principal Skinner.",
-      },
-      {
-        title: "Backend Bandit",
-        date: "2013 - 2015",
-        location: "Comic Book Guy's Software Emporium",
-        description:
-          "Masterminded backend solutions using Node.js and MongoDB. Surprisingly good at debugging, considering my history of causing bugs.",
-      },
-      {
-        title: "Junior Hacker",
-        date: "2010 - 2013",
-        location: "Springfield Elementary Cyber Club",
-        description:
-          "Pioneered early web exploits, leading the club in daring coding escapades. Was disciplined for hacking the school's grading system.",
-      },
-    ],
+    // education: [
+    //   {
+    //     title: "Self-Taught Developer Extraordinaire",
+    //     date: "2000 - Present",
+    //     location: "Springfield Elementary School of Hard Knocks",
+    //     gpa: "4.0 (A+ in Pranks)",
+    //     thesis: "The Art of Subverting Expectations: A Practical Guide",
+    //   },
+    // ],
+    // experience: [
+    //   {
+    //     title: "Frontend Developer",
+    //     date: "2015 - Present",
+    //     location: "Krusty Burger Web Dev",
+    //     description:
+    //       "Crafted interactive web experiences using React and Redux. Often multitasked between coding and avoiding Principal Skinner.",
+    //   },
+    //   {
+    //     title: "Backend Bandit",
+    //     date: "2013 - 2015",
+    //     location: "Comic Book Guy's Software Emporium",
+    //     description:
+    //       "Masterminded backend solutions using Node.js and MongoDB. Surprisingly good at debugging, considering my history of causing bugs.",
+    //   },
+    //   {
+    //     title: "Junior Hacker",
+    //     date: "2010 - 2013",
+    //     location: "Springfield Elementary Cyber Club",
+    //     description:
+    //       "Pioneered early web exploits, leading the club in daring coding escapades. Was disciplined for hacking the school's grading system.",
+    //   },
+    // ],
 
-    skills: ["JavaScript", "React", "Node.js", "MongoDB", "Prank Mastery"], // not used yet
+    // skills: ["JavaScript", "React", "Node.js", "MongoDB", "Prank Mastery"],
   },
 
   projects: [
